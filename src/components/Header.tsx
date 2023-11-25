4a55b75429a14c537bbb44dfaae9191da239f003
Artnai Code <artnaicode@gmail.com>
2023-11-25 20:30:05 +0700

    Fixing UI Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { SocialMedia } from "./SocialMedia";
 
 const Header = () => {
   return (
-    <header className="sticky z-50 bg-yellow-500 dark:bg-gray-900">
+    <header className="sticky z-50 bg-yellow-500 dark:bg-gray-900 px-7">
       <nav className="flex md:flex-row flex-col items-center space-y-3 justify-between py-3 bg-yellow-500 dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
         <div className="hidden md:block">
